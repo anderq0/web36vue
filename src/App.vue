b23ca9edb12574650236c37067e07033cacc971c
Bagdadyan_G <lpsneka@email.com>
2024-08-20 15:02:19 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 @click="str+='!'">{{ str }}</h1>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+  const str = ref('Damn')
+</script>
+
+<style scoped>
+h1{
+  user-select: none;
+}
+</style>

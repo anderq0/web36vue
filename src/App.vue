094d111c50c7093fd30d2584bd2db5b0e908a04f
Bagdadyan_G <lpsneka@email.com>
2024-08-20 15:56:35 +0300

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <h1 @click="str+='!'">{{ str }}</h1>
+  <h1 v-if="bool" @click="str+='!'">{{ str }}</h1>
+  <h1 v-else @click="str+='!'">{{ str.split('').reverse().join('')}}</h1>
+  <button v-on:click="bool=!bool, color+=150" :style="'color:#' + color">Reverse</button>
 </template>
 
 <script setup lang="ts">
   import { ref } from 'vue'
-  const str = ref('Damn')
+  const str = ref('Damn it is vue')
+  const bool = ref(true)
+  const color = ref(123123)
+  
 </script>
 
 <style scoped>
@@ -12,3 +17,4 @@ h1{
   user-select: none;
 }
 </style>
+// https://v3.ru.vuejs.org/ru/guide/introduction.html#декларативная-отрисовка

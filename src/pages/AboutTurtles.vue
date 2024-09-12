0f2f8d8bddd0b5049d655e575829df48687230ad
anderq0 <dwaekkistan@gmail.com>
2024-09-12 21:08:30 +0300

    dfojireoti5u

--- src/pages/AboutTurtles.vue
+++ src/pages/AboutTurtles.vue
@@ -1,9 +1,9 @@
 <template>
   <p>Turtle page</p>
-  <img src="/web36vue/ninjaturtles.webp" alt="" />
+  <img src="/ninjaturtles.webp" alt="" />
   <div class="turtlesBio">
     <div class="character">
-      <img src="/web36vue/leonardo.png" alt="">
+      <img src="/leonardo.png" alt="">
       <p>Leonardo is the calm, focused, and disciplined leader of his brothers, the Teenage Mutant Ninja Turtles. Nicknamed "Leo," he is usually seen with a blue mask and twin swords.</p>
     </div>
   </div>
